d3d62543facbb65d94ff2c81242346ef61b61015
Lilii <lilii-ann.older@voco.ee>
2022-10-13 10:42:03 +0300

    As a user I can add a book [delivers #183530988]

---
+++ app.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form')
+const booksTable = document.querySelector('#books')
+
+const getTitle = document.getElementById('title')
+const getAuthor = document.getElementById('author')
+const getISBN = document.getElementById('isbn')
+const submitBtn = document.getElementById('submit')
+
+
+submitBtn.addEventListener('click', addBook)
+
+function addBook(){
+    let x=document.getElementById('table').insertRow(-1);
+    let titleRow = x.insertCell(0);
+    let authorRow = x.insertCell(1);
+    let isbnRow = x.insertCell(2)
+    let deleteBtn = x.insertCell(3)
+    titleRow.innerHTML = getTitle.value;
+    authorRow.innerHTML = getAuthor.value;
+    isbnRow.innerHTML = getISBN.value;
+    const a = document.createElement('a')
+    a.appendChild(document.createTextNode('X'))
+    a.className = 'blue-text text-darken-2'
+    a.setAttribute('href', '#')
+    deleteBtn.appendChild(a)
+    console.log(x)
+}
